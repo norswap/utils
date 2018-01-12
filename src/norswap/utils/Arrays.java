17bc4e7f58a5a34805119332b87136609b7b78b1
Nicolas "Norswap" Laurent <norswap@gmail.com>
2018-01-12 18:28:32 +0100

    resize_binary_power

--- src/norswap/utils/Arrays.java
+++ src/norswap/utils/Arrays.java
@@ -7,6 +7,8 @@ import java.util.function.Function;
  */
 public final class Arrays
 {
+    // ---------------------------------------------------------------------------------------------
+
     /**
      * Returns an array obtained by applying the function {@code f} to each item in {@code array}.
      */
@@ -17,4 +19,19 @@ public final class Arrays
             out[i] = f.apply(array[i]);
         return out;
     }
+
+    // ---------------------------------------------------------------------------------------------
+
+    /**
+     * Returns a copy of {@code array} whose size is the least power of two greater or equal
+     * to {@code min_size}. Max admissible value for min_size is 2^30.
+     */
+    public static <T> T[] resize_binary_power (T[] array, int min_size)
+    {
+        int size = 1;
+        while (size < min_size) size <<= 1;
+        return java.util.Arrays.copyOf(array, size);
+    }
+
+    // ---------------------------------------------------------------------------------------------
 }
