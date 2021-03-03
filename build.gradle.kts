d45268f41e9377c34c72ad862607cf8c84d1f110
Nicolas "Norswap" Laurent <norswap@gmail.com>
2021-03-03 18:53:27 +0100

    v2.1.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 // === MAIN BUILD DETAILS ==========================================================================
 
 group = "com.norswap"
-version = "2.1.6"
+version = "2.1.7"
 description = "A collection of Java (8+) utilities"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 java.targetCompatibility = JavaVersion.VERSION_1_8
